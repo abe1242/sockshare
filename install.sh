815454da3804a6cc71fd27256306b32ef43cdc5a
Abe <abmdn1242@gmail.com>
2022-10-16 12:03:59 +0530

    Comments in install script

--- install.sh
+++ install.sh
@@ -4,24 +4,30 @@
 
 conprsnt=true
 
+# Checking the program file is present
+# If not, then downloading it from github
 if [ ! -f con ]; then
     conprsnt=false
     echo con binary does not exist, downloading...
     curl -L 'https://raw.githubusercontent.com/abe1242/sockshare/master/con' -o con
 fi
 chmod +x con
-mkdir -p ~/.local/bin
 
+mkdir -p ~/.local/bin
 if ! echo $PATH | grep -q $HOME/.local/bin; then
     echo 'PATH=$PATH:$HOME/.local/bin' >> ~/.profile
 fi
 
+# Copy the program if it was already present
+# Move it otherwise
 if [ $conprsnt == true ]; then
     cp con ~/.local/bin/
 else
     mv con ~/.local/bin/
 fi
 
+# Check if python3 and tqdm are installed on android
+# And check for tqdm on desktop linux
 if [ $OSTYPE == 'linux-android' ]; then
     if dpkg-query -s python >/dev/null 2>/dev/null; then
         echo python3 is installed
